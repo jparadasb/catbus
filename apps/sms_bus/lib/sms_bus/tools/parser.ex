e46c983f471219fd9a2c3caa85502144166d67d9
Jose <jparadas.b@gmail.com>
2021-07-01 00:06:03 -0400

    [ADD] Refactored and support use cases

--- apps/sms_bus/lib/sms_bus/tools/parser.ex
+++ apps/sms_bus/lib/sms_bus/tools/parser.ex
@@ -3,9 +3,10 @@ defmodule SmsBus.Tools.Parser do
 
   @callback to_struct(html :: binary() | {:ok, binary()} | {:error, binary()}) :: [Route.t()]
 
-  @routes_error_selectors "div#servicio_error_solo_paradero, div#respuesta_error_solo_parader"
+  @routes_error_selectors "div#servicio_error_solo_paradero, div#respuesta_error_solo_paradero"
+  @routes_arrivals_selectors "div#siguiente_respuesta, div#proximo_solo_paradero"
 
-  @first_case_selectors "div#siguiente_respuesta, div#proximo_solo_paradero, #{@routes_error_selectors}"
+  @first_case_selectors "#{@routes_arrivals_selectors}, #{@routes_error_selectors}"
   @second_case_selector "div#proximo_respuesta"
   @third_case_selector "div#respuesta_error"
 
@@ -21,19 +22,6 @@ defmodule SmsBus.Tools.Parser do
     []
   end
 
-  # def to_struct(html) do
-  #   with {:ok, document} <- Floki.parse_document(html),
-  #        arrivals_nodes <-
-  #          find_node(document, "div#siguiente_respuesta, div#proximo_solo_paradero"),
-  #        next_arrivals <- get_arrivals_from_nodes(arrivals_nodes),
-  #        error_routes <- get_routes_with_errors_from_nodes(document),
-  #        one_route <- get_arrivals_from_one_route(document) do
-  #     next_arrivals
-  #     |> Enum.concat(error_routes)
-  #     |> Enum.sort_by(&{byte_size(&1.error_message), byte_size(&1.service_number)})
-  #   end
-  # end
-
   def to_struct(html) do
     with {:ok, document} <- Floki.parse_document(html) do
       elements_first_case = find_node(document, @first_case_selectors)
@@ -44,9 +32,14 @@ defmodule SmsBus.Tools.Parser do
     end
   end
 
-  defp build_route_struct(document, first_case, second_case, third_case)
-       when length(first_case) > 0 do
-    "Case 1"
+  defp build_route_struct(document, first_case_nodes, _second_case, _third_case)
+       when length(first_case_nodes) > 0 do
+    %Stop{
+      routes: get_routes(first_case_nodes),
+      code: get_text(document, @stop_code_selector),
+      name: get_text(document, @stop_name_selector),
+      request_time: get_text(document, @stop_request_time)
+    }
   end
 
   defp build_route_struct(document, _first_case, second_case_nodes, _third_case)
@@ -54,7 +47,8 @@ defmodule SmsBus.Tools.Parser do
     %Stop{
       code: get_text(document, @stop_code_selector),
       name: get_text(document, @stop_name_selector),
-      request_time: get_text(document, @stop_request_time)
+      request_time: get_text(document, @stop_request_time),
+      routes: get_routes(document, "div#proximo_respuesta")
     }
   end
 
@@ -82,9 +76,32 @@ defmodule SmsBus.Tools.Parser do
     }
   end
 
-  defp get_arrivals_from_one_route(node) do
-    find_node(node, "div#proximo_respuesta")
-    |> build_next_arrivals
+  defp get_routes(nodes) do
+    find_node(nodes, @routes_arrivals_selectors)
+    |> group_by_route_id()
+    |> build_route()
+    |> Enum.concat(
+      find_node(nodes, @routes_error_selectors)
+      |> get_and_match
+    )
+    |> Enum.sort_by(&{byte_size(&1.error_message), byte_size(&1.service_number)})
+  end
+
+  defp get_routes(node, selector = "div#proximo_respuesta") do
+    [
+      %Route{
+        service_number: get_text(node, "div#numero_parada_respuesta>span:nth-child(5)"),
+        next_arrivals:
+          Floki.find(node, selector)
+          |> Enum.map(fn n ->
+            %NextArrival{
+              arrival_time: get_text(n, "div#proximo_tiempo_respuesta"),
+              distance: get_text(node, "div#proximo_distancia_respuesta"),
+              number_plate: get_text(node, "div#proximo_bus_respuesta")
+            }
+          end)
+      }
+    ]
   end
 
   defp get_routes_with_errors_from_nodes(node) do
--- apps/sms_bus/test/sms_bus_test.exs
+++ apps/sms_bus/test/sms_bus_test.exs
@@ -9,11 +9,10 @@ defmodule SmsBusTest do
   test "CASE 3: stop out operation" do
     SmsBus.Tools.Scraper.Mock
     |> expect(:get_next_arrivals_by, fn _ ->
-      {:ok, File.read!("test/fixtures/case_3.html")}
+      {:ok, File.read!("test/fixtures/third_case.html")}
     end)
 
     assert SmsBus.next_arrivals_by_stop_id("PE513") == %Stop{
-             routes: [],
              name: "Paradero: Palena / esq. Orompello",
              code: "PE513",
              request_time: "21:01 hrs",
@@ -31,7 +30,7 @@ defmodule SmsBusTest do
   test "CASE 2: get_next_arrivals_by when service response with just one route" do
     SmsBus.Tools.Scraper.Mock
     |> expect(:get_next_arrivals_by, fn _ ->
-      {:ok, File.read!("test/fixtures/response_one_service.html")}
+      {:ok, File.read!("test/fixtures/second_case.html")}
     end)
 
     assert SmsBus.next_arrivals_by_stop_id("PE513") == %Stop{
@@ -56,119 +55,124 @@ defmodule SmsBusTest do
   test "CASE 1: get_next_arrivals_by should return and array with routes" do
     SmsBus.Tools.Scraper.Mock
     |> expect(:get_next_arrivals_by, fn _ ->
-      {:ok, File.read!("test/fixtures/response_full.html")}
+      {:ok, File.read!("test/fixtures/first_case.html")}
     end)
 
-    assert SmsBus.next_arrivals_by_stop_id("PC616") ==
-             [
-               %Route{
-                 error_message: "",
-                 service_number: "406C",
-                 next_arrivals: [
-                   %NextArrival{
-                     number_plate: "PFTW-19",
-                     arrival_time: "Entre 07 Y 09 min.",
-                     distance: "2669 mts."
-                   },
-                   %NextArrival{
-                     number_plate: "LXWP-87",
-                     arrival_time: "Entre 12 Y 16 min.",
-                     distance: "4793 mts."
-                   }
-                 ]
-               },
-               %Route{
-                 error_message: "",
-                 service_number: "409",
-                 next_arrivals: [
-                   %NextArrival{
-                     number_plate: "PFVG-73",
-                     arrival_time: "Llegando.",
-                     distance: "65 mts."
-                   },
-                   %NextArrival{
-                     number_plate: "PGBY-62",
-                     arrival_time: "Entre 04 Y 06 min.",
-                     distance: "2118 mts."
-                   }
-                 ]
-               },
-               %Route{
-                 error_message: "",
-                 service_number: "502",
-                 next_arrivals: [
-                   %NextArrival{
-                     number_plate: "FLXG-34",
-                     arrival_time: "Menos de 3 min.",
-                     distance: "573 mts."
-                   }
-                 ]
-               },
-               %Route{
-                 error_message: "",
-                 service_number: "C01",
-                 next_arrivals: [
-                   %NextArrival{
-                     number_plate: "FZJL-69",
-                     arrival_time: "Menos de 4 min.",
-                     distance: "1235 mts."
-                   },
-                   %NextArrival{
-                     number_plate: "CPBF-99",
-                     arrival_time: "Mas de 25 min.",
-                     distance: "6563 mts."
-                   }
-                 ]
-               },
-               %Route{
-                 error_message: "Fuera de horario de operacion para este paradero",
-                 service_number: "C01C",
-                 next_arrivals: []
-               },
-               %Route{
-                 error_message: "",
-                 service_number: "C05",
-                 next_arrivals: [
-                   %NextArrival{
-                     number_plate: "CJRY-50",
-                     arrival_time: "Llegando.",
-                     distance: "309 mts."
-                   }
-                 ]
-               },
-               %Route{
-                 error_message: "",
-                 service_number: "C08",
-                 next_arrivals: [
-                   %NextArrival{
-                     number_plate: "CJRR-11",
-                     arrival_time: "Menos de 3 min.",
-                     distance: "333 mts."
-                   },
-                   %NextArrival{
-                     number_plate: "CJRY-18",
-                     arrival_time: "Entre 09 Y 13 min.",
-                     distance: "4215 mts."
-                   }
-                 ]
-               },
-               %Route{
-                 error_message: "Frecuencia estimada es 1 bus cada 15 min.",
-                 service_number: "C12",
-                 next_arrivals: []
-               },
-               %Route{
-                 error_message: "",
-                 service_number: "C16",
-                 next_arrivals: [
-                   %NextArrival{
-                     number_plate: "CJRK-44",
-                     arrival_time: "Entre 10 Y 14 min.",
-                     distance: "3497 mts."
-                   }
-                 ]
-               }
-             ]
-             |> Enum.sort_by(&{byte_size(&1.error_message), byte_size(&1.service_number)})
+    assert SmsBus.next_arrivals_by_stop_id("PC616") == %Stop{
+             routes:
+               [
+                 %Route{
+                   error_message: "",
+                   service_number: "406C",
+                   next_arrivals: [
+                     %NextArrival{
+                       number_plate: "PFTW-19",
+                       arrival_time: "Entre 07 Y 09 min.",
+                       distance: "2669 mts."
+                     },
+                     %NextArrival{
+                       number_plate: "LXWP-87",
+                       arrival_time: "Entre 12 Y 16 min.",
+                       distance: "4793 mts."
+                     }
+                   ]
+                 },
+                 %Route{
+                   error_message: "",
+                   service_number: "409",
+                   next_arrivals: [
+                     %NextArrival{
+                       number_plate: "PFVG-73",
+                       arrival_time: "Llegando.",
+                       distance: "65 mts."
+                     },
+                     %NextArrival{
+                       number_plate: "PGBY-62",
+                       arrival_time: "Entre 04 Y 06 min.",
+                       distance: "2118 mts."
+                     }
+                   ]
+                 },
+                 %Route{
+                   error_message: "",
+                   service_number: "502",
+                   next_arrivals: [
+                     %NextArrival{
+                       number_plate: "FLXG-34",
+                       arrival_time: "Menos de 3 min.",
+                       distance: "573 mts."
+                     }
+                   ]
+                 },
+                 %Route{
+                   error_message: "",
+                   service_number: "C01",
+                   next_arrivals: [
+                     %NextArrival{
+                       number_plate: "FZJL-69",
+                       arrival_time: "Menos de 4 min.",
+                       distance: "1235 mts."
+                     },
+                     %NextArrival{
+                       number_plate: "CPBF-99",
+                       arrival_time: "Mas de 25 min.",
+                       distance: "6563 mts."
+                     }
+                   ]
+                 },
+                 %Route{
+                   error_message: "Fuera de horario de operacion para este paradero",
+                   service_number: "C01C",
+                   next_arrivals: []
+                 },
+                 %Route{
+                   error_message: "",
+                   service_number: "C05",
+                   next_arrivals: [
+                     %NextArrival{
+                       number_plate: "CJRY-50",
+                       arrival_time: "Llegando.",
+                       distance: "309 mts."
+                     }
+                   ]
+                 },
+                 %Route{
+                   error_message: "",
+                   service_number: "C08",
+                   next_arrivals: [
+                     %NextArrival{
+                       number_plate: "CJRR-11",
+                       arrival_time: "Menos de 3 min.",
+                       distance: "333 mts."
+                     },
+                     %NextArrival{
+                       number_plate: "CJRY-18",
+                       arrival_time: "Entre 09 Y 13 min.",
+                       distance: "4215 mts."
+                     }
+                   ]
+                 },
+                 %Route{
+                   error_message: "Frecuencia estimada es 1 bus cada 15 min.",
+                   service_number: "C12",
+                   next_arrivals: []
+                 },
+                 %Route{
+                   error_message: "",
+                   service_number: "C16",
+                   next_arrivals: [
+                     %NextArrival{
+                       number_plate: "CJRK-44",
+                       arrival_time: "Entre 10 Y 14 min.",
+                       distance: "3497 mts."
+                     }
+                   ]
+                 }
+               ]
+               |> Enum.sort_by(&{byte_size(&1.error_message), byte_size(&1.service_number)}),
+             name: "Paradero: Avenida Las Condes / esq. Sn. Fco. de Asís",
+             code: "PC616",
+             request_time: "14:31 hrs"
+           }
   end
 end
